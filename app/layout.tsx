b104b70164a4ace264b9b4232f78cd2352e56bcc
dehghoon <dehghani.pmp@gmail.com>
2026-08-18 13:27:57 -0400

    Use editor-native shortcuts for project actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import EditorShortcuts from "../components/EditorShortcuts";
 
 export const metadata = {
   title: "Linkoteq 3D Structural Model",
@@ -9,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body><EditorShortcuts />{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
