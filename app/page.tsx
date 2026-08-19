6d87e91fa1b6c17ef93fc27b98015ce0eced0c44
dehghoon <dehghani.pmp@gmail.com>
2026-08-19 00:12:03 -0400

    Wrap 3D editor in LinkoTech site shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import SiteHeader from "../components/SiteHeader";
 
 const StructuralEditor = dynamic(() => import("../components/StructuralEditor"), { ssr: false });
 
 export default function Page() {
-  return <StructuralEditor />;
+  return (
+    <div className="modelSiteShell">
+      <SiteHeader />
+      <div className="modelEditorArea">
+        <StructuralEditor />
+      </div>
+    </div>
+  );
 }
