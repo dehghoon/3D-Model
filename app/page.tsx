60b5cd0f3bd31960bf93c9d821bf28a205603d0f
dehghoon <dehghani.pmp@gmail.com>
2026-08-30 18:38:46 -0400

    Mount contextual helper controller

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import ContextualHelperController from "../components/ContextualHelperController";
 
 const StructuralEditor = dynamic(() => import("../components/StructuralEditor"), { ssr: false });
 
 export default function Page() {
   return (
     <main className="modelEditorArea sapDesktopShell">
+      <ContextualHelperController />
       <StructuralEditor />
     </main>
   );
