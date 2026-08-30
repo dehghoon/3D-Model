f74fa7d6bd88ff73918b3dbed123d94fc47b903f
dehghoon <dehghani.pmp@gmail.com>
2026-08-30 11:20:50 -0400

    Export editor selection foundation

---
+++ lib/editor/index.ts
@@ -0,0 +1,10 @@
+export {
+  clearSelection,
+  createSelection,
+  getSelectionLabel,
+  isSameSelection,
+  reconcileSelection,
+  selectionExists,
+} from "./selection";
+
+export type { EditorSelection } from "./selection";
