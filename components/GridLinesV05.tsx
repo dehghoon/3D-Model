de007cec73a32e7022a2e8f3663ff5fb09887982
dehghoon <dehghani.pmp@gmail.com>
2026-09-01 00:18:39 -0400

    Expose Grid runtime diagnostics

--- components/GridLinesV05.tsx
+++ components/GridLinesV05.tsx
@@ -1,1 +1,14 @@
-export { default } from "./GridLinesV06";
+"use client";
+
+import type { GridLine } from "@linkoteq/structural-core";
+import GridLinesV06 from "./GridLinesV06";
+import GridRuntimeProbeV05 from "./GridRuntimeProbeV05";
+
+export default function GridLinesV05({ grids }: { grids: GridLine[] }) {
+  return (
+    <>
+      <GridLinesV06 grids={grids} />
+      <GridRuntimeProbeV05 grids={grids} />
+    </>
+  );
+}
