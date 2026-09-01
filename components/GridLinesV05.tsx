2e96d3a18b9e9f988e6df6b4f429592cd2ba983d
dehghoon <dehghani.pmp@gmail.com>
2026-09-01 00:32:18 -0400

    Switch Grid renderer to native Three lines

--- components/GridLinesV05.tsx
+++ components/GridLinesV05.tsx
@@ -1,14 +1,1 @@
-"use client";
-
-import type { GridLine } from "@linkoteq/structural-core";
-import GridLinesV06 from "./GridLinesV06";
-import GridRuntimeProbeV05 from "./GridRuntimeProbeV05";
-
-export default function GridLinesV05({ grids }: { grids: GridLine[] }) {
-  return (
-    <>
-      <GridLinesV06 grids={grids} />
-      <GridRuntimeProbeV05 grids={grids} />
-    </>
-  );
-}
+export { default } from "./GridLinesV07";
