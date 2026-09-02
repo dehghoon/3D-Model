d794796fbdf7a9ba19daf396e90c79bd6f210875
dehghoon <dehghani.pmp@gmail.com>
2026-09-02 01:26:07 -0400

    Mount reference status overlay

--- components/StructuralEditorV05.tsx
+++ components/StructuralEditorV05.tsx
@@ -1,1 +1,13 @@
-export { default } from "./StructuralEditorV06";
+"use client";
+
+import StructuralEditorV06 from "./StructuralEditorV06";
+import ReferenceStatusOverlay from "./ReferenceStatusOverlay";
+
+export default function StructuralEditorV05() {
+  return (
+    <div className="structuralEditorViewportHost">
+      <StructuralEditorV06 />
+      <ReferenceStatusOverlay />
+    </div>
+  );
+}
