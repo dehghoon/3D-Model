2c10e824c6cdb60f0b271ba1ab39c0639099fe35
dehghoon <dehghani.pmp@gmail.com>
2026-09-01 20:32:14 -0400

    Wire copy tool into model tools

--- components/LevelGridEditorV05.tsx
+++ components/LevelGridEditorV05.tsx
@@ -1,13 +1,21 @@
 "use client";
 import type { StructuralModel } from "@linkoteq/structural-core";
+import CopyTool from "./CopyToolV05";
 import GridEditor from "./LevelGridEditorV05New";
 import LevelEditor from "./LevelEditorV06";
 
-export default function LevelGridEditorV05({model,onModelChange}:{model:StructuralModel;onModelChange:(model:StructuralModel,status:string)=>void}) {
+export default function LevelGridEditorV05({
+  model,
+  onModelChange,
+}: {
+  model: StructuralModel;
+  onModelChange: (model: StructuralModel, status: string) => void;
+}) {
   return (
     <>
       <GridEditor model={model} onModelChange={onModelChange} />
       <LevelEditor model={model} onModelChange={onModelChange} />
+      <CopyTool model={model} onModelChange={onModelChange} />
     </>
   );
 }
