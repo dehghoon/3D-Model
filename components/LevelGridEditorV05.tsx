c82ef4b946a808777f19dc1c02647de593821dcd
dehghoon <dehghani.pmp@gmail.com>
2026-09-01 22:38:33 -0400

    Mount snapped Copy commit bridge

--- components/LevelGridEditorV05.tsx
+++ components/LevelGridEditorV05.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { StructuralModel } from "@linkoteq/structural-core";
+import CopyInteractionBridgeV01 from "./CopyInteractionBridgeV01";
 import CopyTool from "./CopyToolV05";
 import GridEditor from "./LevelGridEditorV05New";
 import LevelEditor from "./LevelEditorV06";
@@ -13,6 +14,7 @@ export default function LevelGridEditorV05({
 }) {
   return (
     <>
+      <CopyInteractionBridgeV01 model={model} onModelChange={onModelChange} />
       <GridEditor model={model} onModelChange={onModelChange} />
       <LevelEditor model={model} onModelChange={onModelChange} />
       <CopyTool model={model} onModelChange={onModelChange} />
