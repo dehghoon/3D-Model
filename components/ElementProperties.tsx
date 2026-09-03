01a6beeaaa3b48b2a7912e0a2dfe5add442ce44c
dehghoon <dehghani.pmp@gmail.com>
2026-09-02 23:45:38 -0400

    Mount Material quick panel through properties wrapper

--- components/ElementProperties.tsx
+++ components/ElementProperties.tsx
@@ -1,1 +1,31 @@
-export { default } from "./ElementPropertiesV05";
+"use client";
+
+import type { ComponentProps } from "react";
+import ElementPropertiesV05 from "./ElementPropertiesV05";
+import MaterialQuickPanelV05 from "./MaterialQuickPanelV05";
+
+type Props = ComponentProps<typeof ElementPropertiesV05>;
+
+export default function ElementProperties(props: Props) {
+  const selections = props.selections ?? [];
+  const members = selections
+    .filter((item) => item.type === "member")
+    .map((item) => props.model.members.find((member) => member.id === item.id))
+    .filter((item): item is NonNullable<typeof item> => Boolean(item));
+  const surfaces = selections
+    .filter((item) => item.type === "surface")
+    .map((item) => props.model.surfaces.find((surface) => surface.id === item.id))
+    .filter((item): item is NonNullable<typeof item> => Boolean(item));
+
+  return (
+    <>
+      <ElementPropertiesV05 {...props} />
+      <MaterialQuickPanelV05
+        model={props.model}
+        members={members}
+        surfaces={surfaces}
+        onModelChange={props.onModelChange ?? (() => undefined)}
+      />
+    </>
+  );
+}
