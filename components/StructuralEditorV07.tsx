1a7f149dd7c6c4cb1e879d73a8d3e6f0d47d2179
dehghoon <dehghani.pmp@gmail.com>
2026-09-13 21:28:12 -0400

    Fix viewport import after copy bridge mount

--- components/StructuralEditorV07.tsx
+++ components/StructuralEditorV07.tsx
@@ -9,7 +9,7 @@ import LoadManager from "./LoadManager";
 import ModelToolsV05 from "./ModelToolsV05";
 import NodeCreatorV05 from "./NodeCreatorV05";
 import SelectedNodeSupportV05 from "./SelectedNodeSupportV05";
-import ThatOpenViewportV02 from "./ThatOpenViewportV01";
+import ThatOpenViewportV01 from "./ThatOpenViewportV01";
 import { assertCanonicalV05, migrateProjectToV05 } from "../lib/core-v05";
 import { createStarterModelV05 } from "../lib/default-model-v05";
 import { deleteSelection } from "../lib/editor/commands";
